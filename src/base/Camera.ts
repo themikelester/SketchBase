b7737e99ad775612f2fe460cf5fc9efeba18bae1
Mike Lester <themikelester@gmail.com>
2020-12-08 18:55:27 +1100

    Simplified Camera.ts

--- src/base/Camera.ts
+++ src/base/Camera.ts
@@ -5,36 +5,20 @@
 // Date C: 2020/11/27
 //----------------------------------------------------------------------------------------------------------------------
 import { mat4, vec3 } from 'gl-matrix';
-import { defaultValue } from './Util';
 
 export class Camera {
-  cameraMatrix: mat4;
-  viewMatrix: mat4;
-  projMatrix: mat4;
-  projMatrixInverse: mat4;
+  cameraMatrix = mat4.create();
+  viewMatrix = mat4.create();
+  projMatrix = mat4.create();
+  projMatrixInverse = mat4.create();
 
-  viewProjMatrix: mat4;
-  viewProjMatrixInverse: mat4;
+  viewProjMatrix = mat4.create();
+  viewProjMatrixInverse = mat4.create();
 
-  fov: number;
-  near: number;
-  far: number;
-  aspect: number;
-
-  constructor( fovY?: number, aspectRatio?: number, near = 1.0, far = 20000 ) {
-    this.cameraMatrix = mat4.create();
-    this.viewMatrix = mat4.create();
-    this.projMatrix = mat4.create();
-    this.projMatrixInverse = mat4.create();
-
-    this.viewProjMatrix = mat4.create();
-    this.viewProjMatrixInverse = mat4.create();
-
-    this.fov = defaultValue( fovY, 60.0 / 180 * Math.PI );
-    this.near = defaultValue( near, 1.0 );
-    this.far = defaultValue( far, 2000 );
-    this.aspect = defaultValue( aspectRatio, 1 );
-  }
+  fov: number = 60.0 / 180 * Math.PI;
+  near: number = 1.0;
+  far: number = 20000.0;
+  aspect: number = 1;
 
   getFovX(): number {
     const fovx = Math.atan( Math.tan( this.fov * 0.5 ) * this.aspect ) * 2;
